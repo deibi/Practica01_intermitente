e3490a2c45632cc2a5bc7887e6d5446e9961307b
David <papi.maestro@gmail.com>
2019-04-21 21:52:04 +0200

    version definitiva

--- Practica01_intermitente.ino
+++ Practica01_intermitente.ino
@@ -7,7 +7,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrit a fafdae(ledPin, HIGH);
+  digitalWrite(ledPin, HIGH);
   delay(1000);
   digitalWrite   (ledPin, LOW);
   delay(1000);
