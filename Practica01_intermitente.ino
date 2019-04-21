af647523d384d0d3350aaea2d20fcdabbae04091
David <papi.maestro@gmail.com>
2019-04-21 21:47:15 +0200

    tercer commit

--- Practica01_intermitente.ino
+++ Practica01_intermitente.ino
@@ -7,7 +7,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
+  digitalWrit a fafdae(ledPin, HIGH);
   delay(1000);
   digitalWrite   (ledPin, LOW);
   delay(1000);
